1e56f2329ff586ca5c7fe0cf086381c84d59aba4
aegarmus <aegarmus@gmail.com>
2024-10-03 21:37:17 -0300

    update: Setter convertidos en metodos en Gasto

--- assets/js/es6/src/model/Gasto.js
+++ assets/js/es6/src/model/Gasto.js
@@ -18,19 +18,37 @@ export class Gasto {
         return this.#monto
     }
 
-    set nombre(nuevoNombre) {
+    setNombre(nuevoNombre) {
         try {
             this.#nombre = Validar.nombre(nuevoNombre)
+            return {
+              message: "Nombre modificado con éxito",
+              success: true,
+            };
         } catch(error) {
             console.error('No pudimos modificar el nombre del gasto', error)
+            return {
+              success: false,
+              message: "No pudimos modificar el nombre",
+              error,
+            };
         }
     }
 
-    set monto(nuevoMonto) {
+    setMonto(nuevoMonto) {
         try {
-            this.#monto = Validar.monto(nuevoMonto)  
+            this.#monto = Validar.monto(nuevoMonto)
+            return {
+              message: "Nombre modificado con éxito",
+              success: true,
+            };  
         } catch (error) {   
             console.error('No pudimos modificar el monto',  error)
+            return {
+              success: false,
+              message: "No pudimos modificar el nombre",
+              error,
+            };
         }
     }
 
